795ffe2e8f747736c7b6122cdbb3cc171dba3c52
Samuel Vitor <samuelvitor668@gmail.com>
2024-03-14 14:21:10 -0300

    feat: created list in todo-list.ts

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    {
+      task: "minha nova task",
+      checked: true
+    },
+    {
+      task: "minha nova task 2",
+      checked: false
+    }
+  ];
+
+  ngOnInit(): void {
+
+  }
 }
